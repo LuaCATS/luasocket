eb3b99be0a6d41d72612ace90696dba12d2ecc8f
Josef Friedrich <josef@friedrich.rocks>
2023-03-05 17:56:51 +0100

    Add the definition

---
+++ README.md
@@ -0,0 +1,3 @@
+# Definitions for the luasocket library
+
+Type definitions for the [luasocket](https://github.com/lunarmodules/luasocket) library.
---
+++ library/mime.lua
@@ -0,0 +1,33 @@
+---@meta
+
+--- https://lunarmodules.github.io/luasocket/mime.html
+---https://github.com/TeX-Live/luatex/blob/trunk/source/texk/web2c/luatexdir/luasocket/src/mime.lua
+local mime = {}
+
+function mime.b64() end
+
+function mime.decode(name, opt1, opt2) end
+
+function mime.dot() end
+
+function mime.encode(name, opt1, opt2) end
+
+function mime.eol() end
+
+function mime.normalize(marker) end
+
+function mime.qp() end
+
+function mime.qpwrp() end
+
+function mime.stuff() end
+
+function mime.unb64() end
+
+function mime.unqp() end
+
+function mime.wrap(name, opt1, opt2) end
+
+function mime.wrp() end
+
+return mime
